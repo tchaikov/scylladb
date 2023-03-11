d9e8e544aa99b7bceeca349465752acaed38e52e
Kefu Chai <kefu.chai@scylladb.com>
2023-03-11 15:00:02 +0800

    gms/feature_service: introduce UUID_SSTABLE_IDENTIFIERS cluster feature
    UUID_SSTABLE_IDENTIFIERS is a new cluster wide feature. if it is
    enabled, all nodes will generate new sstables with the UUID as their
    generation identifiers. this feature is configured using
    config option of "uuid_sstable_identifiers_enabled".
    
    Signed-off-by: Kefu Chai <kefu.chai@scylladb.com>

--- gms/feature_service.hh
+++ gms/feature_service.hh
@@ -112,6 +112,7 @@ public:
     gms::feature large_collection_detection { *this, "LARGE_COLLECTION_DETECTION"sv };
     gms::feature secondary_indexes_on_static_columns { *this, "SECONDARY_INDEXES_ON_STATIC_COLUMNS"sv };
     gms::feature tablets { *this, "TABLETS"sv };
+    gms::feature uuid_sstable_identifiers { *this, "UUID_SSTABLE_IDENTIFIERS"sv };
 
 public:
 
